9a81b7c8a1395be0ca8fee0adc169dabb6978538
xuminke <xuminke123@gmail.com>
2016-03-07 00:05:40 +0800

    Optimization

--- src/first_component.js
+++ src/first_component.js
@@ -5,15 +5,16 @@ var data_1 = [
 
 var CommentList = React.createClass({
   render: function(){
-    console.log(this.props.data);
-    var commentNodes = this.props.data.map(function(comment){
-      return (
-        <Comment author={comment.author}>{comment.text}</Comment>
-        );
-    });
+    //var commentNodes = this.props.data.map(function(comment){
+    //  return (
+    //    <Comment author={comment.author} key={comment.id} >{comment.text}</Comment>
+    //    );
+    //});
     return (
       <div className="commentList">
-        {commentNodes}
+        {this.props.data.map(function(comment){
+          return (<Comment author={comment.author} key={comment.id} >{comment.text}</Comment>);
+        })}
       </div>
       );
   }
